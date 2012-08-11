3e719a0508f9e33f2a88bc03d651b5d9c8022b17
Peter Lejeck <peter.lejeck@gmail.com>
2012-08-11 15:52:39 -0700

    Compile user script and install cwVer.js

---
+++ cwVer.js
@@ -0,0 +1,10 @@
+cwVer.callbacks['e461889525ef40c4c0f634d294e981ff']({
+	'name': 'Bubbles',
+	'version': '0.2.0',
+	'icon': 'http://i49.tinypic.com/2n6989w.png',
+	'downloads': {
+		'chrome': 'http://nuckchorris.github.com/Bubbles/Bubbles.user.js',
+		'firefox': 'http://nuckchorris.github.com/Bubbles/Bubbles.user.js',
+		'safari': 'http://nuckchorris.github.com/Bubbles/Bubbles.user.js'
+	}
+});
